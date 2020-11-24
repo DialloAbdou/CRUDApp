45a8a19f24f2779c167ccb5f9a0092836c9d2a15
DialloAbdou <abdoudiallo3826@yahoo.fr>
2020-11-24 15:54:19 +0100

    Initialisation BD

--- CRUDApp/Migrations/20201124130741_InitData.Designer.cs
+++ CRUDApp/Migrations/20201124130741_InitData.Designer.cs
@@ -3,14 +3,16 @@ using CRUDApp.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CRUDApp.Migrations
 {
     [DbContext(typeof(CrudDbContext))]
-    partial class CrudDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201124130741_InitData")]
+    partial class InitData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CRUDApp/Migrations/20201124130741_InitData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUDApp.Migrations
+{
+    public partial class InitData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Personnes",
+                columns: new[] { "PersonneId", "Adresse", "CodePostal", "Mail", "Nom", "Prenom", "Telephone", "Ville" },
+                values: new object[,]
+                {
+                    { 1, "Allée Reaumure", 26000, "dialloabdou26@gmail.com", "DIALLO", "Abdou", "06 16 43 28 73", "Valence" },
+                    { 2, "5C Eugene Varlin", 93000, "barryami26@gmail.com", "Barry", "Aminata", "06 13 43 75 83", "Montreuil" },
+                    { 3, "3 martin Guemou", 13000, "alybah26@gmail.com", "Bah", "Aly", "06 13 66 75 23", "Marseil" },
+                    { 4, "13 allée clotures", 33000, "MSow26@gmail.com", "Sow", "Moussa", "06 43 53 15 73", "Grenoble" },
+                    { 5, "13 allée des Martirs", 73000, "Mcamara26@gmail.com", "Camara", "Amara", "06 43 15 15 73", "Paris" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Personnes",
+                keyColumn: "PersonneId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Personnes",
+                keyColumn: "PersonneId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Personnes",
+                keyColumn: "PersonneId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Personnes",
+                keyColumn: "PersonneId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Personnes",
+                keyColumn: "PersonneId",
+                keyValue: 5);
+        }
+    }
+}
